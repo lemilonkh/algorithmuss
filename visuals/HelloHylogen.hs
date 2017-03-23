c4877cdf8bc8f71d2c621c70315411478cf99408
Milan Gruner <milangruner@gmail.com>
2017-03-23 17:31:29 +0100

    :lipstick: Changes HelloHylogen visuals Color and trigonometric functions changed

--- visuals/HelloHylogen.hs
+++ visuals/HelloHylogen.hs
@@ -9,12 +9,12 @@ output :: Program
 output = toProgram color
 
 color :: Vec4
-color = vec4(a*0.3, a*0.9, a*0.6, 1)
+color = vec4(a*0.7, a*0.2, a*0.3, 1)
   where
     k = 40
     f = (*k) . sin . cos . tan . (/k)
     a = sum [
       cos(x_ uvN * f time + x_ mouse),
       sin(y_ uvN * f time + y_ mouse),
-      exp(x_ uvN * f time + x_ mouse),
-      exp(y_ uvN * f time + y_ mouse)]
+      atan(x_ uvN * f time + x_ mouse),
+      tanh(y_ uvN * f time + y_ mouse)]
